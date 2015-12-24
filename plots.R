c6c02d66e7a977f663e32a487b6af149cc5d672a
3manuek <3manuek@esdebian.org>
2015-12-24 19:02:02 -0300

    http://stackoverflow.com/questions/19706320/create-a-data-frame-using-text-input-in-shiny

--- plots.R
+++ plots.R
@@ -16,7 +16,7 @@ con <- dbConnect(MySQL(),
                  ,dbname="test", socket="/tmp/mysql_sandbox14901.sock", 
                  host="127.0.0.1")
 
-word <- "country"
+word <- "find"
 
 #f<-function(x) {as.character(input$filtering)}
 
@@ -54,25 +54,32 @@ GROUP BY sourceTable
 rs <- dbGetQuery(con, sql)
 
 
+QE <- "WITH QUERY EXPANSION"
+# QE <- ""
+
 sql <- paste("
 SELECT \"BC\" as sourceTable, FTS_DOC_ID, match(BC.content) against (\"" ,f(x) ," \" IN BOOLEAN MODE) as scoreBoolean,
-       match(BC.content) against (\""  ,f(x), "\" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) scoreQE,
+       match(BC.content) against (\""  ,f(x), "\" IN NATURAL LANGUAGE MODE ",QE,") scoreQE,
        length(content) as lengthContent
 FROM
         bookContent BC
 WHERE
-  match(BC.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
+  match(BC.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE ",QE,")
 UNION
 SELECT \"BL\" as sourceTable, FTS_DOC_ID, match(BL.content) against (\"",f(x)," \" IN BOOLEAN MODE) as scoreBoolean,
-       match(BL.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) scoreQE,
+       match(BL.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE ",QE,") scoreQE,
        length(content) as lengthContent
 FROM
         bookContentByLine BL
 WHERE
-  match(BL.content) against (\"", f(x) ,"\" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
+  match(BL.content) against (\"", f(x) ,"\" IN NATURAL LANGUAGE MODE ",QE,")
 ")
 
 rs <- dbGetQuery(con, sql)
 
+pl1 <- qplot(scoreBoolean,scoreQE, data = rs, fill = factor(sourceTable), geom = c("boxplot") )
+
+#preOj <- preProcess(rs$scoreQE, method = c("center","scale"))  
+#featurePlot(x=rs[,c("scoreQE","lengthContent")], y=rs$scoreBoolean)
 
 
