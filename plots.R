6244aede82912cfa5655a13a274cde6e1c8d7f18
3manuek <3manuek@esdebian.org>
2015-12-24 15:33:54 -0300

    Added new entries and plots

---
+++ plots.R
@@ -0,0 +1,78 @@
+require(RMySQL)
+library(ggplot2)
+require(caret)
+
+
+source('~/git/fts_article/load.R')
+
+# Yeah, only for development environements
+all_cons <- dbListConnections(MySQL())
+for(con in all_cons) dbDisconnect(con)
+
+
+# You do not want clear passwords, but is a sandbox.
+con <- dbConnect(MySQL(), 
+                 user="msandbox", password="msandbox", port=14901 
+                 ,dbname="test", socket="/tmp/mysql_sandbox14901.sock", 
+                 host="127.0.0.1")
+
+word <- "country"
+
+#f<-function(x) {as.character(input$filtering)}
+
+f <- function(x) {as.character(word)}
+
+sql <- paste("SELECT sourceTable, sum(scoreBoolean) as sumBoolean, 
+        sum(scoreQE) as sumQE,
+        max(scoreBoolean) as maxBoolean,
+        max(scoreQE) as maxBoolean,
+        avg(scoreBoolean) as avgBoolean, 
+        avg(scoreQE) as avgQE,
+        avg(lengthContent) avgLength,
+        avg(scoreQE) / count(sourceTable) as weight,
+        count(sourceTable) as countOccurrences
+             FROM
+             (
+             SELECT \"BC\" as sourceTable, FTS_DOC_ID, match(BC.content) against (\"" ,f(x) ," \" IN BOOLEAN MODE) as scoreBoolean,
+       match(BC.content) against (\""  ,f(x), "\" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) scoreQE,
+       length(content) as lengthContent
+FROM
+        bookContent BC
+WHERE
+  match(BC.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
+UNION
+SELECT \"BL\" as sourceTable, FTS_DOC_ID, match(BL.content) against (\"",f(x)," \" IN BOOLEAN MODE) as scoreBoolean,
+       match(BL.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) scoreQE,
+       length(content) as lengthContent
+FROM
+        bookContentByLine BL
+WHERE
+  match(BL.content) against (\"", f(x) ,"\" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
+) unionTable
+GROUP BY sourceTable
+;" , sep = "")
+rs <- dbGetQuery(con, sql)
+
+
+sql <- paste("
+SELECT \"BC\" as sourceTable, FTS_DOC_ID, match(BC.content) against (\"" ,f(x) ," \" IN BOOLEAN MODE) as scoreBoolean,
+       match(BC.content) against (\""  ,f(x), "\" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) scoreQE,
+       length(content) as lengthContent
+FROM
+        bookContent BC
+WHERE
+  match(BC.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
+UNION
+SELECT \"BL\" as sourceTable, FTS_DOC_ID, match(BL.content) against (\"",f(x)," \" IN BOOLEAN MODE) as scoreBoolean,
+       match(BL.content) against (\"" ,f(x)," \" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION) scoreQE,
+       length(content) as lengthContent
+FROM
+        bookContentByLine BL
+WHERE
+  match(BL.content) against (\"", f(x) ,"\" IN NATURAL LANGUAGE MODE WITH QUERY EXPANSION)
+")
+
+rs <- dbGetQuery(con, sql)
+
+
+
